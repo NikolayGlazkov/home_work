7b8657f069b174ab19cb779dceb8e3b5b1dd949a
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 01:20:30 +0600

    Enden Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int n;
+int last_digit = 0;
+Console.Write("Enter a number: ");
+n = int.Parse(Console.ReadLine());
+
+last_digit = n % 10;
+
+    Console.WriteLine($"Последнее число {last_digit}");
+
