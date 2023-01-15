92f8d4211185198da5230ad4e108307180b9c505
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 00:43:51 +0600

    end Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+
+
+Console.WriteLine("введите число");
+int num1 = int.Parse(Console.ReadLine());
+
+if(num1 %2 == 0)
+{
+    Console.WriteLine($"чесло являеться четным");
+}
