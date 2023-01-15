a608c9e103d3e1659a77e6847e164e7baffbf1cd
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 00:26:25 +0600

    ferst task end

---
+++ Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите число №1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число №2");
+int num2 = int.Parse(Console.ReadLine());
+
+int exponent = 2;
+double result = Math.Pow(num2, exponent);
+Console.WriteLine(result);
+
+
+if(num1 == result)
+{
+    Console.WriteLine($"Число №1: {num1} евляеться квадратом второго: {num2}");
+}
+if(num1 != result)
+{
+    Console.WriteLine($"Число №1: {num1} не евляеться квадратом второго: {num2}");
+}
