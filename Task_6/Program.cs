1fdf917cb3a858a4a9f7577b6bd2ecf1b25bf8a3
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 01:43:52 +0600

    Enden Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int n;
+Console.Write("Enter a number: ");
+n = int.Parse(Console.ReadLine());
+
+int secondDigit = (n / 10) % 10;
+Console.WriteLine(secondDigit);
