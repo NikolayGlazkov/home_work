24410f15c11828083b45bfe5f599d2277b318834
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 00:37:37 +0600

    end task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("введите число");
+int num1 = int.Parse(Console.ReadLine());
+
+
+
+if(num1 == 1)
+{
+    Console.WriteLine($"текщий день недели понедельник");
+}
+if(num1 == 2)
+{
+    Console.WriteLine($"текщий день недели вторник");
+}
+if(num1 == 3)
+{
+    Console.WriteLine($"текщий день недели среда");
+}
+if(num1 == 4)
+{
+    Console.WriteLine($"текщий день недели четверг");
+}
+if(num1 == 5)
+{
+    Console.WriteLine($"текщий день недели пятница");
+}
+if(num1 == 6)
+{
+    Console.WriteLine($"текщий день недели субота");
+}
+if(num1 == 7)
+{
+    Console.WriteLine($"текщий день недели воскресние");
+}
