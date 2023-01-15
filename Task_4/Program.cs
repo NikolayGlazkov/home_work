62d8ee745e5e7d9cf9baaaffd1bb298c4e5db440
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 00:59:01 +0600

    Ended Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+int n;
+Console.Write("Enter a number: ");
+n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+
+if(i % 2 == 0)
+{
+    Console.WriteLine($"чесло {i} являеться четным");
+}
