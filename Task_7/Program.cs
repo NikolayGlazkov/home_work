93bccb4a2c201bb73cd55d5fe039d9596c191908
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 12:52:57 +0600

    Ended Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+
+int n = 99;
+int maxDigit = 0;
+
+for (int i = 10; i <= n; i++)
+
+{
+if(i > maxDigit)
+    maxDigit = i;
+}
+
+Console.WriteLine("Max digit is: " + maxDigit);
