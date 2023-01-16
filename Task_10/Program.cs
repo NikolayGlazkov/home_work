805f14a42603aee281f649965f00416d9ca4f615
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 22:26:57 +0600

    ended Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+
+int num1;
+int thirdDigit;
+
+Console.Write("Enter a number №1:");
+num1 = int.Parse(Console.ReadLine());
+thirdDigit = num1 % 10;
+
+if(num1 <= 99)
+{
+    Console.WriteLine($" в заданном числе 2 цифры");
+}
+if(num1 >= 100)
+{
+    Console.WriteLine($"третья цифра числа: {thirdDigit}");
+}
