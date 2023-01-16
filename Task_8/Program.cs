d087b827cbae0d81286c2e37c4a1a6c0e372ff67
NikolayGlazkov <gn9377419582@gmail.com>
2023-01-16 13:06:19 +0600

    Ended Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,7 @@
+﻿int num;
+Console.Write("Enter a number: ");
+num = int.Parse(Console.ReadLine());
+int firstDigit = num / 100; // Получаем первую цифру
+int thirdDigit = num % 10; // Получаем третью цифру
+int newNum = firstDigit * 10 + thirdDigit; // Собираем новое число
+Console.WriteLine(newNum); // Выводим новое число
